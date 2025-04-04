f36ef8ad945daac0207d44eea28670125fb614f0
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2025-04-04 15:18:02 -0400

    feat(UI): responsive grid layout with drag and drop functionality
    Fixes: #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+import DashboardLayout from "@/components/DashboardLayout";
+import GridItem from "@/components/GridItem";
+import ReactGridLayout from "react-grid-layout";
 
 export default function Home() {
+  // TODO: read layouts from local storage or provide the follows as default
+  const layoutLG: ReactGridLayout.Layout[] = [
+    { i: "card-2", x: 0, y: 0, w: 4, h: 1 },
+    { i: "card-3", x: 0, y: 1, w: 3, h: 2.5 },
+    { i: "card-6", x: 3, y: 2.5, w: 1, h: 2.5 },
+    { i: "card-4", x: 0, y: 5, w: 3, h: 8 },
+    { i: "card-7", x: 3, y: 5, w: 1, h: 5 },
+    { i: "card-8", x: 3, y: 5, w: 1, h: 2 },
+    { i: "card-9", x: 3, y: 5, w: 1, h: 1 },
+    { i: "card-5", x: 0, y: 13, w: 3, h: 8 },
+    { i: "card-10", x: 3, y: 13, w: 1, h: 8 },
+  ];
+  const layoutSM: ReactGridLayout.Layout[] = [
+    { i: "card-2", x: 0, y: 0, w: 2, h: 1 },
+    { i: "card-3", x: 0, y: 1, w: 1, h: 1.5 },
+    { i: "card-6", x: 1, y: 2.5, w: 1, h: 1.5 },
+    { i: "card-4", x: 0, y: 4, w: 1, h: 6 },
+    { i: "card-7", x: 1, y: 4, w: 1, h: 3 },
+    { i: "card-8", x: 1, y: 4, w: 1, h: 2 },
+    { i: "card-9", x: 1, y: 4, w: 1, h: 1 },
+    { i: "card-5", x: 0, y: 10, w: 1, h: 6 },
+    { i: "card-10", x: 1, y: 10, w: 1, h: 6 },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex min-h-screen flex-col md:flex-row">
+      <header>header</header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <DashboardLayout
+        layouts={{
+          lg: layoutLG,
+          md: layoutLG,
+          sm: layoutSM,
+          xs: layoutSM,
+          xxs: layoutSM,
+        }}
+      >
+        <GridItem key="card-2" handlePosition="topRight">
+          2
+        </GridItem>
+        <GridItem key="card-3" handlePosition="topRight">
+          3
+        </GridItem>
+        <GridItem key="card-4" handlePosition="topRight">
+          4
+        </GridItem>
+        <GridItem key="card-5" handlePosition="topRight">
+          5
+        </GridItem>
+        <GridItem key="card-6" handlePosition="topRight">
+          6
+        </GridItem>
+        <GridItem key="card-7" handlePosition="topRight">
+          7
+        </GridItem>
+        <GridItem key="card-8" handlePosition="topRight">
+          8
+        </GridItem>
+        <GridItem key="card-9" handlePosition="topRight">
+          9
+        </GridItem>
+        <GridItem key="card-10" handlePosition="topRight">
+          10
+        </GridItem>
+      </DashboardLayout>
+      <footer className="h-9">footer</footer>
+    </main>
   );
 }
